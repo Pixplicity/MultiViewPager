e752df7f50e3c4778b9b8d73202cf8cd672f84e3
Paul Lammertsma <paul@pixplicity.com>
2014-09-15 14:28:36 +0200

    Tweaked layout

--- test/src/com/pixplicity/multiviewpager/PagerActivity.java
+++ test/src/com/pixplicity/multiviewpager/PagerActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class PagerActivity extends FragmentActivity {
 
@@ -14,6 +17,10 @@ public class PagerActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pager);
+
+        TextView tvInfo = (TextView) findViewById(R.id.tv_info);
+        tvInfo.setMovementMethod(LinkMovementMethod.getInstance());
+        tvInfo.setText(Html.fromHtml(getResources().getString(R.string.info_link)));
 
         mPager = (MultiViewPager) findViewById(R.id.pager);
 
